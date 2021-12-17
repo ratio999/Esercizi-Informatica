78e5c67d633a64e24fb639c98730a75470291df3
Giuseppe Troian <64421588+ratio999@users.noreply.github.com>
2021-12-17 08:47:23 +0000

    Esercizi su liste

---
+++ src/main2.py
@@ -0,0 +1,15 @@
+print("Esercizi list")
+l=[]
+import math
+k=["Giuseppe","Troian"]
+print(k[1])
+m=[0,1,2,3,4]
+print(m[0:3])
+print(m[-4])
+m[0:3]
+print(m)
+a=range(8)
+b=[int((x*3)+math.pi) for x in a]
+c=[x*3 for x in a]
+print(b[0:6])
+print(c[0:6])
